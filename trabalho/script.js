91563e05b9416079f694ddeee034a90702b4a3f5
Marco Miguel Borba <mmborba@furb.br>
2026-03-19 07:56:18 -0300

    Adicionado o botão para a tela de login

--- trabalho/script.js
+++ trabalho/script.js
@@ -1,3 +1,3 @@
-document.getElementById("button_login").addEventListener("click", function () {
-    window.location.href = "login.html";
+document.getElementById("Ref_Login").addEventListener("click", function () {
+    window.location.href = "Login.html";
 });
