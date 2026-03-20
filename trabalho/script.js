ea931432b9e23c02730e2da8704845380ccbe117
Lucas Lino da Costa Pinto <llcpinto@furb.br>
2026-03-20 11:01:58 -0300

    botao que faz excluir uma linha da tabela

--- trabalho/script.js
+++ trabalho/script.js
@@ -7,9 +7,21 @@ function inicio(){
     alert("oi");
     console.log("Inicio");
 }
-function excluir(params) {
-    
-}
+function excluir() {
+    let numero = prompt("Qual linha quer excluir? (começa em 1)");
+
+    let tabela = document.getElementById("tabela");
+  
+    // +1 porque a linha 0 é o cabeçalho
+    let index = parseInt(numero);
+  
+    if (!isNaN(index) && tabela.rows[index]) {
+      tabela.deleteRow(index);
+    } else {
+      alert("Linha inválida!");
+    }
+  }
+
 
 function mostrarSucesso(event) {
     event.preventDefault();
