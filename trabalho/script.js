2a190a2e940444f01b2c44baca439c9eb62206fd
Marco Miguel Borba <mmborba@furb.br>
2026-03-20 08:10:17 -0300

    Retirado o Head e deixado apenas o menu, implementação do botão voltar na tela de login

--- trabalho/script.js
+++ trabalho/script.js
@@ -1,3 +1,8 @@
 document.getElementById("Ref_Login").addEventListener("click", function () {
     window.location.href = "Login.html";
 });
+
+function inicio(){
+    alert("oi");
+    console.log("Inicio");
+}
