59b4dfaa1857136dba2b23b5cf47db85d8ad076f
Lucas Lino da Costa Pinto <llcpinto@furb.br>
2026-03-20 09:43:56 -0300

    arrumei a logo no menu

--- trabalho/script.js
+++ trabalho/script.js
@@ -2,7 +2,7 @@ document.getElementById("Ref_Login").addEventListener("click", function () {
     window.location.href = "Login.html";
 });
 
-<<<<<<< HEAD
+
 function inicio(){
     alert("oi");
     console.log("Inicio");
@@ -10,9 +10,9 @@ function inicio(){
 function excluir(params) {
     
 }
-=======
+
 function mostrarSucesso(event) {
     event.preventDefault();
     document.getElementById('modal-sucesso').style.display = 'block';
 }
->>>>>>> a88c1969b7d37f243633ef9de27c515ae4c222bd
+
